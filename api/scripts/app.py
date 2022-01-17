bf8a0d43e76689e8bb949d6080fbab897f946422
e205723 <e205723@ie.u-ryukyu.ac.jp>
2022-01-17 19:07:59 +0900

    feat(api): repeat what the sender says

--- api/scripts/app.py
+++ api/scripts/app.py
@@ -2,16 +2,10 @@ from flask import Flask, jsonify
 from flask import request
 import gpt2_model
 
-device = gpt2_model.load_device()
-tokenizer = gpt2_model.generate_tokenizer()
-pretrained_model_path = 'Yoshisaur/kono-chat'
-model = gpt2_model.generate_model(pretrained_model_path, device)
-
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 @app.route('/reply', methods=['POST'])
 def reply():
     message = request.json['message']
-    reply = gpt2_model.generate_reply(message, device, tokenizer, model)
-    return jsonify({'message': reply})
+    return jsonify({'message': message})
