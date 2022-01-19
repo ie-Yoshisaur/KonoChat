31023340d3e4390338efa8dcace4cd7c1a020f33
Yugo Nishimura <e195719@ie.u-ryukyu.ac.jp>
2022-01-19 10:48:18 +0900

    feat(api): get reply from gpt2

--- api/scripts/app.py
+++ api/scripts/app.py
@@ -5,7 +5,14 @@ import gpt2_model
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+device = gpt2_model.load_device()
+tokenizer = gpt2_model.generate_tokenizer()
+pretrained_model_path = 'Yoshisaur/kono-chat'
+model = gpt2_model.generate_model(pretrained_model_path, device)
+
+
 @app.route('/reply', methods=['POST'])
 def reply():
     message = request.json['message']
-    return jsonify({'message': message})
+    reply = gpt2_model.generate_reply(message, device, tokenizer, model)
+    return jsonify({'message': reply})
